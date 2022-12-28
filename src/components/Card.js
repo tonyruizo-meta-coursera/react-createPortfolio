9c71417775f2b4014b1a3454898bf591d150ee20
Tony Ruiz <tonyruizo@pm.me>
2022-12-28 09:25:19 -0500

    Fix card component UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, Box, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -8,21 +8,25 @@ const Card = ({ title, description, imageSrc }) => {
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-    <VStack>
-      <div>
-        <Image src={imageSrc} />
-        <Heading as="h5" size="sm">
+    <VStack bg="white" borderRadius="5px" m={2} align="flex-start">
+      <Image borderRadius="5px" src={imageSrc} />
+      <Box p={2}>
+        <Heading as="h5" size="sm" color="black" my={2}>
           {title}
         </Heading>
-        <Text fontSize="sm">{description}</Text>
-        <div>
-          <a href="/">
-            <Text fontSize="xs">
-              See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
-            </Text>
-          </a>
-        </div>
-      </div>
+      </Box>
+      <Box p={2}>
+        <Text fontSize="sm" color="gray.600">
+          {description}
+        </Text>
+      </Box>
+      <Box p={2}>
+        <a href="/">
+          <Text fontSize="xs" color="black" fontWeight="extrabold" my={2}>
+            See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
+          </Text>
+        </a>
+      </Box>
     </VStack>
   );
 };
